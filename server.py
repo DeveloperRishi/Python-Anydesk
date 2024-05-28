27922ca31581ddf2231d3c28dd67117d238e6194
Rishi Rao <61234147+DeveloperRishi@users.noreply.github.com>
2024-05-28 06:16:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import time
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9999))
+    server_socket.bind(('localhost', 9999)) # Replace with your Server Computer IP
     server_socket.listen(1)
 
     print("Server listening on port 9999...")
